1cf5da28ede3e416315bc2ec743411ad397e3965
ibukub <ibukunatolagbe@gmail.com>
2022-06-22 02:26:35 +0100

    django crude

---
+++ I4G007911WDU/blog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+# Create your views here.
+from django.views.generic.list import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+from django.views.generic.edit import CreateView
+from .models import Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+
+success_url  =  reverse_lazy('blog:all')   
+
+from django.views.generic.detail import DetailView
+from .models import Post
+
+class PostDetailView(DetailView):
+
+    model = Post
+    fields = '__all__'
+
+success_url  =  reverse_lazy('blog:all')   
+
+from django.views.generic.edit import UpdateView
+from .models import Post
+
+class PostUpdateView(UpdateView): 
+    model = Post
+    fields = '__all__'
+
+success_url  =  reverse_lazy('blog:all') 
+
+from django.views.generic.edit import UpdateView
+from .models import Post
+
+class PostDeleteView(UpdateView): 
+    model = Post
+    fields = '__all__'
+
+success_url  =  reverse_lazy('blog:all') 
